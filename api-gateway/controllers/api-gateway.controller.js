2affb2fa49182dd517ac8611154155cd40c198ab
ndogotaru <nicolae.dogotaru@gmail.com>
2023-08-24 14:26:38 +0200

    clean the repo

--- api-gateway/controllers/api-gateway.controller.js
+++
@@ -1,7 +0,0 @@
-// get api-gateway
-const getApiGateway = async (req, res) => {
-  console.log("Hello api gateway");
-  return res.status(200).json({ response: "Hello api gateway !" });
-};
-
-module.exports = { getApiGateway };
